d20e95e827ab757c4cea44fd9bf3820013addd89
seanlee0923 <leesinue0923@gmail.com>
2024-04-21 22:31:49 +0900

    feat: nats 서버 기본 설정
    기초 설정, 이후에 sub, pub 기능 추가 필요함

---
+++ models/nats/nats.go
@@ -0,0 +1,43 @@
+package nats
+
+import (
+	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+type natsRequest struct {
+	postId  uint
+	message string
+}
+
+type natsResponse struct {
+	Conn         *nats.Conn
+	Subscription *nats.Subscription
+}
+
+type NatsClient struct {
+	Conn          *nats.Conn
+	Subs          map[string]*nats.Subscription
+	UniqueReplyTo map[string]string
+	ReqTimeout    time.Duration
+}
+
+func makeNatsClient() *NatsClient {
+	natsName := os.Getenv("NATS_HOSTNAME")
+	natsPort := os.Getenv("NATS_PORT")
+
+	natsUrl := "nats://" + natsName + ":" + natsPort
+	natsConn, err := nats.Connect(natsUrl)
+
+	if err != nil {
+		panic(err)
+	}
+	connect := &NatsClient{
+		Conn:          natsConn,
+		Subs:          make(map[string]*nats.Subscription),
+		UniqueReplyTo: make(map[string]string),
+		ReqTimeout:    time.Second * 5,
+	}
+}
