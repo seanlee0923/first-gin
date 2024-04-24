72f4ff5df5cce5b08d2ac8fbdee8bd9abce71463
seanlee0923 <leesinue0923@gmail.com>
2024-04-24 18:45:33 +0900

    fix: natsConn->encodedConn
    fix: natsConn->encodedConn

--- models/nats/nats.go
+++ models/nats/nats.go
@@ -14,16 +14,21 @@ type Post struct {
 	Regdate time.Time
 }
 
-func makeNatsClient() *nats.Conn {
+func NatsConnection() (*nats.Conn, *nats.EncodedConn) {
 	natsName := os.Getenv("NATS_HOSTNAME")
 	natsPort := os.Getenv("NATS_PORT")
 
 	natsUrl := "nats://" + natsName + ":" + natsPort
 	natsConn, err := nats.Connect(natsUrl)
-
 	if err != nil {
 		panic(err)
 	}
+	encodedConn, _ := nats.NewEncodedConn(natsConn, nats.JSON_ENCODER)
+	return natsConn, encodedConn
+}
 
-	return natsConn
+func publishPost(topic string, post Post) {
+	_, encodedConn := NatsConnection()
+
+	encodedConn.Publish(topic, "")
 }
